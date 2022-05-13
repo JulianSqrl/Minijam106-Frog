5ae159703793e1a34a71024ed0e3b4e67fa04d3e
JulianSqrl <82903600+JulianSqrl@users.noreply.github.com>
2022-05-13 18:52:46 +0100

    Added abiltiy to swing on tongue
    hold mouse button to swing

--- FrogGame/Assets/Scripts/TongueShoot.cs
+++ FrogGame/Assets/Scripts/TongueShoot.cs
@@ -5,7 +5,22 @@ using UnityEngine;
 public class TongueShoot : MonoBehaviour
 {
 
-    private Transform cameraReference;
+    bool grappling = false;
+
+    public LineRenderer lineRenderer;
+
+    public Transform cameraReference;
+    public Transform playerReference;
+    public Transform frogModelReference;
+
+
+    public LayerMask TongueAtachable;
+
+
+    Vector3 swingPosition;
+
+
+    SpringJoint joint;
 
 
 
@@ -14,8 +29,15 @@ public class TongueShoot : MonoBehaviour
     }
 
 
+    void LateUpdate()
+    {
+        DrawTongue();
+    }
+
     void Update()
     {
+
+
         //this defines when the grapple is shot
         if(Input.GetMouseButtonDown(0))
         {
@@ -28,14 +50,88 @@ public class TongueShoot : MonoBehaviour
 
     }
 
+
+
+
+
+
+
+
+    
+
+    void LaunchTongue()
+    {
+        //launch a physics object toward the direction of the camera and link the swing function to it
+
+    }
+
+    void RetractTongue()
+    {
+        //launches the tongue back toward the player and when colliding with the player deletes it
+
+    }
+
+
+
+    void DrawTongue()
+    {
+        if(grappling)
+        {
+            lineRenderer.SetPosition(0,frogModelReference.position);
+            lineRenderer.SetPosition(1,swingPosition);
+        }
+    }
+
+
+
     void StartGrapple()
     {
+        //print(cameraReference.forward);
+        RaycastHit hit;
+        //print(cameraReference.position);
+        if(Physics.Raycast(origin:cameraReference.position,direction:cameraReference.forward,out hit,300f,TongueAtachable))
+        {
+            SpringJoint joint;
+            //this is so the stop grapple has no errors
+            if(playerReference.gameObject.TryGetComponent<SpringJoint>(out joint) == false)
+            {
+                joint=playerReference.gameObject.AddComponent<SpringJoint>();
+            }
 
+
+            swingPosition = hit.point;
+            joint.autoConfigureConnectedAnchor = false;
+            joint.connectedAnchor = swingPosition;
+
+            float tongueDistance = Vector3.Distance(playerReference.position,swingPosition);
+
+            joint.maxDistance = tongueDistance*0.8f;
+            joint.minDistance = tongueDistance *0.25f;
+
+            joint.spring = 4.5f;
+            joint.damper = 7f;
+            joint.massScale = 4.5f;
+            
+            //so the line renderer can work properlly
+            lineRenderer.positionCount = 2;
+            grappling = true;
+            
+        }
     }
 
     void StopGrapple()
     {
+        //so the tongue derenders
+        lineRenderer.positionCount = 0;
 
+
+        if(playerReference.gameObject.TryGetComponent<SpringJoint>(out joint))
+        {
+            joint.breakForce = 0f;
+            joint = null;
+        }
+
+        grappling = false;
     }
 
 
